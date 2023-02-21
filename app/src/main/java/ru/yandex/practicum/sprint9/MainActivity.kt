d425ab0009ec18225d6fa2a8cc960fb0d8ff3f45
Aleksei Anisimov <suxelshock@gmail.com>
2023-02-21 22:14:57 +0300

    Бинарные маски

--- app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun Int.toBinary(): String {
+        return String.format("%8s", Integer.toBinaryString(this)).replace(' ', '0')
     }
 }
