efc810ff40ef27de1b449a6414a4e0ab75c00ad3
Aleksei Anisimov <suxelshock@gmail.com>
2023-02-21 22:57:05 +0300

    Множественные темы

--- app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.yandex.practicum.sprint9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
