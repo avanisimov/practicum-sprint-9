a6e72a8106c670ee1bf8afd9e26873a69e04c1a4
Aleksei Anisimov <suxelshock@gmail.com>
2023-02-22 20:07:30 +0300

    Множественные темы

--- app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
@@ -2,17 +2,38 @@ package ru.yandex.practicum.sprint9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var currentTheme = R.style.Theme_Sprint9
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("SPRINT_9", "MainActivity $this $savedInstanceState")
+        setTheme(currentTheme)
         setContentView(R.layout.activity_main)
 
+//        findViewById<Button>(R.id.default_theme).setOnClickListener {
+//            currentTheme = R.style.Theme_Sprint9
+//
+//            recreate()
+//        }
+//
+//        findViewById<Button>(R.id.alternate_theme).setOnClickListener {
+//            currentTheme = R.style.Theme_Sprint9_Alternate
+//            recreate()
+//        }
+
+        val image = findViewById<ImageView>(R.id.image)
+        Log.d("SPRINT_9", "image $image")
 
     }
+
 }
