e2cfe3176cf7732595d9ca499953021b316ab9a3
Aleksei Anisimov <suxelshock@gmail.com>
2023-02-22 19:34:13 +0300

    Бинарные маски

--- app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
@@ -1,13 +1,25 @@
 package ru.yandex.practicum.sprint9
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val uiMode: Int = resources.configuration.uiMode
+
+        val masked = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        Log.d("SPRINT_9", "uiMode $uiMode")
+        Log.d("SPRINT_9", "$uiMode -> ${uiMode.toBinary()}")
+        Log.d("SPRINT_9", "${Configuration.UI_MODE_NIGHT_MASK} -> ${Configuration.UI_MODE_NIGHT_MASK.toBinary()}")
+        Log.d("SPRINT_9", "${Configuration.UI_MODE_NIGHT_YES} -> ${Configuration.UI_MODE_NIGHT_YES.toBinary()}")
+        Log.d("SPRINT_9", "${masked} -> ${masked.toBinary()}")
+        Log.d("SPRINT_9", "${masked == Configuration.UI_MODE_NIGHT_NO}")
     }
 
     private fun Int.toBinary(): String {
