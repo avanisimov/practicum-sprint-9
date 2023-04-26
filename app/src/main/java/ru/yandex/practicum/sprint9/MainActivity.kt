d98f689dcef7401f725b45f398f0f8af74557b7b
Aleksei Anisimov <suxelshock@gmail.com>
2023-04-26 20:40:45 +0300

    Примеры разных тем и стилей

--- app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint9/MainActivity.kt
@@ -11,8 +11,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(ThemeManager.currentTheme)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.classic).setOnClickListener {
+            ThemeManager.currentTheme = R.style.Theme_Sprint9
+            recreate()
+        }
+        findViewById<Button>(R.id.alternative).setOnClickListener {
+            ThemeManager.currentTheme = R.style.Theme_Sprint9_Alternative
+            recreate()
+        }
 
     }
+}
+
+object ThemeManager {
+    var currentTheme: Int = R.style.Theme_Sprint9
 }
